5fd96a6cb99a07a8286222226cc08595fd440976
paula <p.andrescaldevilla@gmail.com>
2021-04-22 19:09:01 +0200

    sin bonus

--- models/coaster.model.js
+++ models/coaster.model.js
@@ -2,7 +2,34 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const coasterModel = new Schema({
-    /* --- */
+   name: {
+    type: String,
+    required: true
+   },
+    description: {
+        type: String,
+        default: 'Not defined'
+    },
+    inversions: {
+        type: Number,
+        default: 0
+    },
+    length: {
+        type: Number,
+        default: 0
+    },
+    active: {
+        type: Boolean,
+        default: true
+    },
+    park_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Park'
+    }
 })
 
-module.exports = mongoose.model('Coaster', coasterModel)
+//module.exports = mongoose.model('Coaster', coasterModel)
+
+const Coaster = mongoose.model('Coaster', coasterModel)
+
+module.exports = Coaster
--- models/park.model.js
+++ models/park.model.js
@@ -2,7 +2,25 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const parkSchema = new Schema({
-    /* --- */
+    name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 50,
+        trim: true
+    },
+    description: {
+        type: String,
+        require: true,
+        minlength: 2,
+        maxlength: 500,
+        trim: true
+    },
+    active: {
+        type: Boolean,
+        default: true
+    }
+    
 })
-
-module.exports = mongoose.model('Park', parkSchema)
+const Park = mongoose.model('Park', parkSchema)
+module.exports = Park
